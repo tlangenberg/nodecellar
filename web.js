ff6310a6525ac07c73e9da89f39b84dc4a37afe9
tlangenberg <thomas.langenberg@gmail.com>
2013-11-18 23:48:21 +0100

    Update web.js

--- web.js
+++ web.js
@@ -1,31 +1,10 @@
-var express = require('express'),
-    path = require('path'),
-    http = require('http'),
-    wine = require('./routes/wines');
-
+var express = require('express');
 var app = express();
+app.use(express.logger());
 
-app.configure(function () {
-    app.set('port', process.env.PORT || 3000);
-    app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
-    app.use(express.bodyParser()),
-    app.use(express.static(path.join(__dirname, 'public')));
-	
-});
+app.use(express.static(__dirname + '/public'));
 
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-app.get('/wines', wine.findAll);
-app.get('/wines/populate', wine.populate);
-app.get('/wines/:id', wine.findById);
-app.post('/wines', wine.addWine);
-app.put('/wines/:id', wine.updateWine);
-app.delete('/wines/:id', wine.deleteWine);
-
-http.createServer(app).listen(app.get('port'), function () {
-    console.log("Express server listening on port " + app.get('port'));
-});
-
-
