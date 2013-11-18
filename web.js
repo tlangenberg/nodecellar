8e31bc53bd94e6d436e6d9bc67b08be9082e8de9
tlangenberg <thomas.langenberg@gmail.com>
2013-11-18 23:22:54 +0100

    Update and rename server.js to web.js

--- web.js
+++ web.js
@@ -10,9 +10,13 @@ app.configure(function () {
     app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
     app.use(express.bodyParser()),
     app.use(express.static(path.join(__dirname, 'public')));
+	app.listen(port, function() {
+	  console.log("Listening on " + port);
+	});	
 });
 
 app.get('/wines', wine.findAll);
+app.get('/wines/populate', wine.populate);
 app.get('/wines/:id', wine.findById);
 app.post('/wines', wine.addWine);
 app.put('/wines/:id', wine.updateWine);
@@ -21,3 +25,5 @@ app.delete('/wines/:id', wine.deleteWine);
 http.createServer(app).listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port'));
 });
+
+
