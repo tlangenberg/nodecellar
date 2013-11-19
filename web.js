92006735b922466175774840e87afd8259bd7cbc
tlangenberg <thomas.langenberg@gmail.com>
2013-11-19 01:40:05 +0100

    Update web.js

--- web.js
+++ web.js
@@ -1,35 +1,15 @@
 var express = require('express'),
     path = require('path'),
     http = require('http'),
-    io = require('socket.io'),
     wine = require('./routes/wines');
 
 var app = express();
 
 app.configure(function () {
     app.set('port', process.env.PORT || 3000);
-    app.use(express.logger('dev'));
-    app.use(express.bodyParser())
+    app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser()),
     app.use(express.static(path.join(__dirname, 'public')));
-    //app.use(express.static(__dirname + '/public'));    
-});
-
-var server = http.createServer(app);
-io = io.listen(server);
-
-
-io.configure(function () {
-    io.set('authorization', function (handshakeData, callback) {
-        if (handshakeData.xdomain) {
-            callback('Cross-domain connections are not allowed');
-        } else {
-            callback(null, true);
-        }
-    });
-});
-
-server.listen(app.get('port'), function () {
-    console.log("Express server listening on port " + app.get('port'));
 });
 
 app.get('/wines', wine.findAll);
@@ -38,18 +18,6 @@ app.post('/wines', wine.addWine);
 app.put('/wines/:id', wine.updateWine);
 app.delete('/wines/:id', wine.deleteWine);
 
-io.sockets.on('connection', function (socket) {
-
-    socket.on('message', function (message) {
-        console.log("Got message: " + message);
-        ip = socket.handshake.address.address;
-        url = message;
-        io.sockets.emit('pageview', { 'connections': Object.keys(io.connected).length, 'ip': '***.***.***.' + ip.substring(ip.lastIndexOf('.') + 1), 'url': url, 'xdomain': socket.handshake.xdomain, 'timestamp': new Date()});
-    });
-
-    socket.on('disconnect', function () {
-        console.log("Socket disconnected");
-        io.sockets.emit('pageview', { 'connections': Object.keys(io.connected).length});
-    });
-
+http.createServer(app).listen(app.get('port'), function () {
+    console.log("Express server listening on port " + app.get('port'));
 });
