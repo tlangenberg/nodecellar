a0057e37b8a7c29c7aec980bc2ca11a7be64d9ae
tlangenberg <thomas.langenberg@gmail.com>
2013-11-19 01:44:34 +0100

    Update web.js

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ app.configure(function () {
     app.set('port', process.env.PORT || 3000);
     app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
     app.use(express.bodyParser()),
-    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static(__dirname + '/public'));
 });
 
 app.get('/wines', wine.findAll);
