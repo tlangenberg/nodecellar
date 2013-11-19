b7a70ecba9de76d0eed41724910ba46fa2080f78
tlangenberg <thomas.langenberg@gmail.com>
2013-11-19 01:04:11 +0100

    Update web.js

--- web.js
+++ web.js
@@ -1,17 +1,17 @@
 var express = require('express'),
     path = require('path'),
     http = require('http'),
-    io = require('socket.io'),    
+    io = require('socket.io'),
     wine = require('./routes/wines');
 
 var app = express();
 
 app.configure(function () {
     app.set('port', process.env.PORT || 3000);
-    app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
-    app.use(express.bodyParser()),
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser())
     //app.use(express.static(path.join(__dirname, 'public')));
-    app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + '/public'));    
 });
 
 var server = http.createServer(app);
@@ -31,17 +31,6 @@ io.configure(function () {
 server.listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port'));
 });
-
-//app.use(express.logger());
-
-//app.use(express.static(__dirname + '/public'));
-
-//var port = process.env.PORT || 5000;
-
-//app.listen(port, function() {
-//  console.log("Listening on " + port);
-//});
-
 
 app.get('/wines', wine.findAll);
 app.get('/wines/:id', wine.findById);
