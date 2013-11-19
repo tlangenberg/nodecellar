52e16b0cae158437ed18582490e1afc8d4cbbe57
tlangenberg <thomas.langenberg@gmail.com>
2013-11-19 01:39:17 +0100

    Update wines.js

--- routes/wines.js
+++ routes/wines.js
@@ -5,7 +5,7 @@ var Server = mongo.Server,
     BSON = mongo.BSONPure;
 
 //var server = new Server('localhost', 27017, {auto_reconnect: true});
-var server = new Server('mongodb://OpLqFhclNYPi:MvqgABFMwMoB@dbs001.mongosoup.de/cc_OpLqFhclNYPi', {auto_reconnect: true});
+var server = new Server('mongodb://OpLqFhclNYPi:MvqgABFMwMoB@dbs001.mongosoup.de/cc_OpLqFhclNYPi', 19176, {auto_reconnect: true});
 db = new Db('winedb', server, {safe: true});
 
 console.log("db connectin established");
