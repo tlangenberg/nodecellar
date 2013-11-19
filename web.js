67762b8c85be014095de6c820a197d32e98f3c2f
tlangenberg <thomas.langenberg@gmail.com>
2013-11-19 01:09:13 +0100

    Update web.js

--- web.js
+++ web.js
@@ -10,8 +10,8 @@ app.configure(function () {
     app.set('port', process.env.PORT || 3000);
     app.use(express.logger('dev'));
     app.use(express.bodyParser())
-    //app.use(express.static(path.join(__dirname, 'public')));
-    app.use(express.static(__dirname + '/public'));    
+    app.use(express.static(path.join(__dirname, 'public')));
+    //app.use(express.static(__dirname + '/public'));    
 });
 
 var server = http.createServer(app);
